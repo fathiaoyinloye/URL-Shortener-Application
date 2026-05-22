c283ff305b8a97ac5c1a730f2495a9dd156ac425
fathiaoyinloye <fathiaoyinloye21@gmail.com>
2026-05-22 15:25:30 +0100

    chore: allow vercel config

--- Url-shortner/src/main/java/com/urlshortner/CorsConfig.java
+++ Url-shortner/src/main/java/com/urlshortner/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://*.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
