827ab993d97af9605aa1bc2ffddaff2a089d8e1a
fathiaoyinloye <fathiaoyinloye21@gmail.com>
2026-05-24 07:33:13 +0100

    cors update

--- Url-shortner/src/main/java/com/urlshortner/CorsConfig.java
+++ Url-shortner/src/main/java/com/urlshortner/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://*.vercel.app"
+                                "https://url-shortener-frontend-eosin.vercel.app/"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
